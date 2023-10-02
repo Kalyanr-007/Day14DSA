05ee69488ae87a2038b53a0058a4953c737736a5
kalyan <kalyanr444@gmail.com>
2023-10-02 16:20:20 +0530

    UC4complete.

--- Day14DSA/src/datastructures/Main.java
+++ Day14DSA/src/datastructures/Main.java
@@ -2,14 +2,16 @@ package datastructures;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome Data Structure Problems using Java Generics Project.");
+        System.out.println("Wellcome Data Structure Problems using Java Generics Project.");
 
         LinkedListGenerics<Integer> lgList = new LinkedListGenerics<Integer>();
 
         lgList.pushValue(70);
-        lgList.pushValue(30);
         lgList.pushValue(56);
 
+        lgList.displayList();
+
+        lgList.insertValue(30,56);
         lgList.displayList();
     }
 }
